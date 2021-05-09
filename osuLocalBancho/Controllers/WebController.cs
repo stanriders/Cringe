04c5de99af97b040ec04c50079ff7dece5894acf
StanR <hi@stanr.info>
2021-05-09 22:49:55 +0300

    Split score submission into a service

--- osuLocalBancho/Controllers/WebController.cs
+++ osuLocalBancho/Controllers/WebController.cs
@@ -1,15 +1,16 @@
 ﻿using System;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Org.BouncyCastle.Crypto.Engines;
 using Org.BouncyCastle.Crypto.Modes;
 using Org.BouncyCastle.Crypto.Paddings;
 using Org.BouncyCastle.Crypto.Parameters;
-using osuLocalBancho.Bancho;
+using osuLocalBancho.Bancho.Packets;
 using osuLocalBancho.Database;
 using osuLocalBancho.Services;
 using osuLocalBancho.Types;
-using osuLocalBancho.Types.Enums;
 
 namespace osuLocalBancho.Controllers
 {
@@ -18,10 +19,12 @@ namespace osuLocalBancho.Controllers
     public class WebController : ControllerBase
     {
         private readonly BanchoService _banchoService;
+        private readonly ScoreService _scoreService;
 
-        public WebController(BanchoService banchoService)
+        public WebController(BanchoService banchoService, ScoreService scoreService)
         {
             _banchoService = banchoService;
+            _scoreService = scoreService;
         }
 
         [HttpGet("bancho_connect.php")]
@@ -34,46 +37,13 @@ namespace osuLocalBancho.Controllers
         }
 
         [HttpPost("osu-submit-modular-selector.php")]
-        public IActionResult SubmitScore([FromForm] string score, [FromForm] string iv, [FromForm] string osuver, [FromForm] string x, [FromForm] string ft)
+        public async Task<IActionResult> SubmitScore([FromForm] string score, [FromForm] string iv, [FromForm] string osuver, [FromForm(Name = "x")] string quit, [FromForm(Name = "ft")] string failed)
         {
-            var key = "h89f2-890h2h89b34g-h80g134n90133";
-            if (!string.IsNullOrEmpty(osuver))
+            var submittedScore = await _scoreService.SubmitScore(score, iv, osuver, quit == "1", failed == "1");
+            if (submittedScore != null)
             {
-                key = $"osu!-scoreburgr---------{osuver}";
-            }
-
-            var decodedData = Convert.FromBase64String(score);
-            var decodedIv = Convert.FromBase64String(iv);
-
-            var scoreData = DecryptScoreData(decodedData, decodedIv, Encoding.Default.GetBytes(key));
-            if (scoreData.Length >= 16 && scoreData[0].Length == 32)
-            {
-                var quit = x == "1";
-                using var scoreDb = new ScoreDatabaseContext();
-                scoreDb.Scores.Add(new SubmittedScore
-                {
-                    FileMd5 = scoreData[0],
-                    PlayerUsername = scoreData[1],
-                    // %s%s%s = scoreData[2]
-                    Count300 = int.Parse(scoreData[3]),
-                    Count100 = int.Parse(scoreData[4]),
-                    Count50 = int.Parse(scoreData[5]),
-                    CountGeki = int.Parse(scoreData[6]),
-                    CountKatu = int.Parse(scoreData[7]),
-                    CountMiss = int.Parse(scoreData[8]),
-                    Score = int.Parse(scoreData[9]),
-                    MaxCombo = int.Parse(scoreData[10]),
-                    FullCombo = scoreData[11] == "True",
-                    Rank = scoreData[12],
-                    Mods = int.Parse(scoreData[13]),
-                    Passed = scoreData[14] == "True",
-                    GameMode = int.Parse(scoreData[15]),
-                    //PlayDateTime = DateTime.Now,//int.Parse(scoreData[16]),
-                    OsuVersion = scoreData[17],
-                    Quit = quit,
-                    Failed = !quit && int.Parse(ft) > 0,
-                });
-                scoreDb.SaveChanges();
+                // send score as a notif to confirm submission
+                _banchoService.EnqueuePacket(new Notification(submittedScore.Id.ToString()));
 
                 var outData = "beatmapId:1|beatmapSetId:2|beatmapPlaycount:3|beatmapPasscount:2|approvedDate:0\n" +
                               new Chart
@@ -90,15 +60,16 @@ namespace osuLocalBancho.Controllers
                                   AccuracyAfter = 0.33f,
                                   PpBefore = 666,
                                   PpAfter = 727,
-                              } +
+                              }
+                              +
                               new Chart
                               {
                                   Type = "overall",
                                   Name = "AYE",
                                   RankBefore = 2,
                                   RankAfter = 1,
-                                  ScoreBefore = 100,
-                                  ScoreAfter = 200,
+                                  //ScoreBefore = player.TotalScore,
+                                  //ScoreAfter = player.TotalScore + (ulong)submittedScore.Score,
                                   ComboBefore = 1,
                                   ComboAfter = 5,
                                   AccuracyBefore = 0.2f,
@@ -107,12 +78,8 @@ namespace osuLocalBancho.Controllers
                                   PpAfter = 727,
                               };
 
-                // send score as a notif to confirm submission
-                _banchoService.EnqueuePacket(DataPacket.PackData($"{scoreData[9]}"), ServerPacketType.Notification);
-
                 return new OkObjectResult(outData);
             }
-
             return new OkResult();
         }
 
@@ -121,22 +88,6 @@ namespace osuLocalBancho.Controllers
         {
             var data = $"2|false|1488|{i}|1\n0\naye\n10\n";
             return new OkObjectResult(data);
-        }
-
-        private string[] DecryptScoreData(byte[] score, byte[] iv, byte[] key)
-        {
-            var engine = new RijndaelEngine(256);
-            var blockCipher = new CbcBlockCipher(engine);
-            var cipher = new PaddedBufferedBlockCipher(blockCipher, new ZeroBytePadding());
-            var keyParam = new KeyParameter(key);
-            var keyParamWithIv = new ParametersWithIV(keyParam, iv, 0, 32);
-
-            cipher.Init(false, keyParamWithIv);
-            var comparisonBytes = new byte[cipher.GetOutputSize(score.Length)];
-            var length = cipher.ProcessBytes(score, comparisonBytes, 0);
-            cipher.DoFinal(comparisonBytes, length);
-
-            return Encoding.Default.GetString(comparisonBytes).Split(':');
         }
     }
 }

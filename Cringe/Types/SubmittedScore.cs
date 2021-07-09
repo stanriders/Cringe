a39c59a55cf3abf90794297f8e7c568017e90fc7
StanR <hi@stanr.info>
2021-07-09 23:21:19 +0300

    Add beatmap leaderboards

--- Cringe/Types/SubmittedScore.cs
+++ Cringe/Types/SubmittedScore.cs
@@ -1,5 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
-using Newtonsoft.Json;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Cringe.Types
 {
@@ -8,8 +8,6 @@ namespace Cringe.Types
         [Key] public int Id { get; set; }
 
         public string FileMd5 { get; set; }
-
-        public string PlayerUsername { get; set; }
 
         // %s%s%s = scoreData[2]
         public int Count300 { get; set; }
@@ -26,13 +24,21 @@ namespace Cringe.Types
         public bool Passed { get; set; }
         public int GameMode { get; set; }
 
-        public int PlayDateTime { get; set; }
+        public DateTime PlayDateTime { get; set; }
 
         //self.calculateAccuracy()
         public string OsuVersion { get; set; }
         public bool Quit { get; set; }
         public bool Failed { get; set; }
         public double Pp { get; set; }
+
         public Player Player { get; set; }
+
+        public override string ToString()
+        {
+            //18204696(scoreId)|sendlolipls(username)|167060(score/pp)|663(combo)|0(50)|6(100)|483(300)|0(miss)|6(katu)|79(geki)|False(fc)|216(mods)|101029(???)|4(rank)|1596985749(date)|1(??)
+            return $"{Id}|{Player.Username}|{Score}|{MaxCombo}|{Count50}|{Count100}|{Count300}|{CountMiss}|{CountKatu}|{CountGeki}|{FullCombo}|{Mods}|1|1|{PlayDateTime:yyMMddhhmmss}|1\n";
+            
+        }
     }
 }

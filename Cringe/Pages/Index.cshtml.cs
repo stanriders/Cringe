d4a0f58e3592e7f946d882fb72250532968cf663
StanR <hi@stanr.info>
2021-07-10 03:54:55 +0300

    Add biggest cringe to the home page

--- Cringe/Pages/Index.cshtml.cs
+++ Cringe/Pages/Index.cshtml.cs
@@ -1,12 +1,30 @@
 
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cringe.Database;
+using Cringe.Types;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace Cringe.Pages
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        private readonly PlayerDatabaseContext _playerContext;
+        private readonly ScoreDatabaseContext _scoreContext;
+
+        public IndexModel(PlayerDatabaseContext playerContext, ScoreDatabaseContext scoreContext)
         {
+            _playerContext = playerContext;
+            _scoreContext = scoreContext;
+        }
+
+        public IList<SubmittedScore> Scores { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            Scores = await _scoreContext.Scores.OrderByDescending(x => x.Pp).Take(100).ToListAsync();
         }
     }
 }

e3a578c6d9b9db93ed905c8eb79a595ebd11fe26
StanR <hi@stanr.info>
2021-08-04 16:57:34 +0300

    Don't throw on failed pp calc

--- Cringe/Services/PpService.cs
+++ Cringe/Services/PpService.cs
@@ -1,6 +1,8 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Threading.Tasks;
 using Cringe.Types.Database;
+using Microsoft.Extensions.Logging;
 using OppaiSharp;
 using Beatmap = OppaiSharp.Beatmap;
 
@@ -9,22 +11,34 @@ namespace Cringe.Services
     public class PpService
     {
         private readonly BeatmapService _beatmapService;
+        private readonly ILogger<PpService> _logger;
 
-        public PpService(BeatmapService beatmapService)
+        public PpService(BeatmapService beatmapService, ILogger<PpService> logger)
         {
             _beatmapService = beatmapService;
+            _logger = logger;
         }
 
         public async Task<double> CalculatePp(ScoreBase score)
         {
-            await using var stream = new MemoryStream(await _beatmapService.GetBeatmapBytes(score.BeatmapId), false);
-            using var reader = new StreamReader(stream, true);
+            try
+            {
+                await using var stream =
+                    new MemoryStream(await _beatmapService.GetBeatmapBytes(score.BeatmapId), false);
+                using var reader = new StreamReader(stream, true);
 
-            var map = Beatmap.Read(reader);
-            var diff = new DiffCalc().Calc(map, (Mods) score.Mods);
+                var map = Beatmap.Read(reader);
+                var diff = new DiffCalc().Calc(map, (Mods) score.Mods);
 
-            return new PPv2(new PPv2Parameters(map, diff, score.Accuracy / 100, score.CountMiss, score.MaxCombo,
-                (Mods) score.Mods)).Total;
+                return new PPv2(new PPv2Parameters(map, diff, score.Accuracy / 100, score.CountMiss, score.MaxCombo,
+                    (Mods) score.Mods)).Total;
+
+            }
+            catch (Exception e)
+            {
+                _logger.LogError($"PP calculation failed: {e}");
+                return 0.00;
+            }
         }
     }
 }
